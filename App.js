fe77908e60064a0fb111bd1f3203513febd01fef
stagiaireBoss <149372718+stagiaireBoss@users.noreply.github.com>
2023-11-20 16:45:50 +0100

    A

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Camera } from 'react-native-vision-camera';
 
 export default function App() {
   return (
